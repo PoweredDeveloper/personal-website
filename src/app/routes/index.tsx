1d94c9999940e0a952d44f3b41eeb348cf6d3436
Mikis <orokana.otaku.dev@gmail.com>
2025-12-08 03:53:23 +0300

    feat: ascii image preview

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,20 +1,41 @@
 import AsciiImage from '@/components/AsciiImage'
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect, useRef, useState } from 'react'
 
 export const Route = createFileRoute('/')({
   component: Portfolio,
 })
 
+const IMAGES = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
 function Portfolio() {
+  const [image, setImage] = useState<string>('9.jpeg')
+  const imageIndex = useRef<number>(0)
+
+  useEffect(() => {
+    setInterval(() => {
+      setImage(`${IMAGES[imageIndex.current]}.jpeg`)
+      imageIndex.current++
+    }, 5000)
+  }, [])
+
   return (
     <>
       <div className="h-screen w-full">
         <AsciiImage
-          imageUrl="/11.jpeg"
+          imageUrl={image}
           options={{
             display: {
-              rows: 80,
-              fontSize: 10,
+              rows: 120,
+              fontSize: 6,
+            },
+            animation: {
+              animated: true,
+              revealMode: 'empty',
+              duration: 4500,
+            },
+            imageProcessing: {
+              contrast: 1.1,
             },
           }}
         />
